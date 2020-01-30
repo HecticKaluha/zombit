973ca0460decd24c045d8889c282867ce9237444
Stefano Verhoeve <sverhoeve@davinci.nl>
2020-01-30 22:20:24 +0100

    made use of classes for router
    todo: find right flow for the use of classes router and core, (maybe save instance of core in router).
    
    Maybe use a GameManager?

--- public/index.php
+++ public/index.php
@@ -2,11 +2,13 @@
 define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 
 require(ROOT . '/packages/RedBeanPHP5_4_2/rb.php');
+
+//make use of classes
+//find out the flow
+
 require(ROOT . "/core/config.php");
 R::setup('mysql:host=localhost;dbname='. DB_NAME, DB_USER, DB_PASS);
 R::useFeatureSet( 'novice/latest' );
-
-require(ROOT . "/core/route.php");
+require(ROOT . "/core/Router.php");
 require(ROOT . "/core/core.php");
-
-route();
+Router::getInstance()->route();

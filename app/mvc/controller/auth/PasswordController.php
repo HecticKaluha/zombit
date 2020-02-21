8014094ac2b5658d4fbd7e326f736ddbfd436b8d
Stefano Verhoeve <sverhoeve@davinci.nl>
2020-02-21 19:05:04 +0100

    added functionallity to reset your password
    todo: check if the mail sends correctly.
    Send an actual link that redirects to server, and ends up on a form to reset your password

--- app/mvc/controller/auth/PasswordController.php
+++ app/mvc/controller/auth/PasswordController.php
@@ -1,24 +1,62 @@
 <?php
-class PasswordController{
-    static function index(){
+
+class PasswordController
+{
+    static function index()
+    {
         Core::render(PARTIALS . 'auth/forgotPassword.php');
     }
 
-    static function resetPassword(){
+    static function resetPassword()
+    {
         $request = new ResetPasswordRequest($_POST);
         $data = $request->getData();
-        if($request->isValid()){
+        if ($request->isValid()) {
             //mail the user a link to reset the password
 
+            // Multiple recipients
+            $to = $data['email']; // note the comma
 
-            Core::render(PARTIALS . '/auth/passwordReset.php', array('data' => $data));
-        }
-        else {
+            // Subject
+            $subject = 'Reset uw wachtwoord voor Zombit';
+
+            // Message
+            $message = '
+                        <html>
+                        <head>
+                          <title>Reset uw wachtwooord voor zombit</title>
+                        </head>
+                        <body>
+                          <h1>Reset je wachtoord voor Zombit.</h1>
+                          <p>Klik op de onderstaande link om uw wachtwoord te resetten.</p>
+                          <a href="#">Link hier</a>
+                        </body>
+                        </html>
+                        ';
+
+            // To send HTML mail, the Content-type header must be set
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+            // Additional headers
+            $headers[] = 'To: ' . $data['email'];
+            $headers[] = 'From: support@zombit.noreply.com';
+
+            // Mail it
+            if(mail($to, $subject, $message, implode("\r\n", $headers))){
+                Core::render(PARTIALS . '/auth/passwordReset.php', array('data' => $data));
+            }
+            else{
+                $errors = array("email" => array("Het is niet gelukt om een mail te sturen naar dit email. Probeer het later opnieuw."));
+                Core::render(PARTIALS . '/auth/forgotPassword.php', array('data' => $data, 'errors' => $errors));
+            }
+        } else {
             Core::render(PARTIALS . 'auth/forgotPassword.php', array('data' => $data, 'errors' => $request->getErrors()));
         }
     }
 
-    static function logOut(){
+    static function logOut()
+    {
 
     }
 

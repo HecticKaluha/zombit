481d397ef5fc637b44f5368a9d666cc207a6712a
Stefano Verhoeve <sverhoeve@davinci.nl>
2020-02-21 14:29:39 +0100

    changed some styling

--- app/mvc/view/partials/home.php
+++ app/mvc/view/partials/home.php
@@ -1,8 +1,19 @@
 <div id="description" class="flex flex-row scrollable">
-    <p>Een post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soort</p>
-    <img class="" src="<?= SOURCES ?>/img/henk.png">
+    <p>Een post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soort</p>
+    <div>
+        <img class="" src="<?= SOURCES ?>/img/henk.png">
+
+    </div>
 </div>
 
 <div id="options" class="flex flex-column scrollable">
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
     <a href="<?= URL ?>level/show/1">Nothing here yet</a>
 </div>

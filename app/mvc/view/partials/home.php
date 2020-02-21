d683b12245f15a8f734676902f179867ac379ff7
Stefano Verhoeve <sverhoeve@davinci.nl>
2020-02-21 18:16:55 +0100

    fixed homepage

--- app/mvc/view/partials/home.php
+++ app/mvc/view/partials/home.php
@@ -1,5 +1,5 @@
 <div id="description" class="flex flex-row scrollable">
-    <p>Een post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soortEen post apocalyptische textbased RPG. De eerste in zijn soort</p>
+    <p>Een post apocalyptische textbased RPG. De eerste in zijn soort. In dit spel creër je je eigen verhaal. Bevecht zombies, verazamel unieke wapens, en probeer te overleven. Let op, je hebt maar één kans! Maak een weloverwogen keuze en bepaal jouw toekomst. </p>
     <div>
         <img class="" src="<?= SOURCES ?>/img/henk.png">
 
@@ -7,8 +7,6 @@
 </div>
 
 <div id="options" class="flex flex-column scrollable">
-    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
-    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
-    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
-    <a href="<?= URL ?>level/show/1">Nothing here yet</a>
+    <a href="<?= URL ?>register/create">Maak een account</a>
+    <a href="<?= URL ?>login">Login</a>
 </div>
